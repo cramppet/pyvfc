d883698aa341d487666f6615092fa1e6c04cd5f2
Peter Crampton <pwc42@drexel.edu>
2020-02-29 14:00:55 -0500

    Fixing OpenCV versioning problems for PyPi

--- setup.py
+++ setup.py
@@ -112,8 +112,8 @@ setup(
     description='Vector field consensus (VFC) Python wrapper',
     long_description='',
     ext_modules=ext_modules,
-    install_requires=['pybind11>=2.4', 'opencv-python'],
-    setup_requires=['pybind11>=2.4', 'opencv-python'],
+    install_requires=['pybind11>=2.4', 'opencv-python==3.4.9.31'],
+    setup_requires=['pybind11>=2.4', 'opencv-python==3.4.9.31'],
     cmdclass={'build_ext': BuildExt},
     zip_safe=False,
     classifiers=[
